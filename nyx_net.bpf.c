06582bf3ab4029074a2bf879442ddf9bba1698e1
TheSNMC <274265874+thesnmc@users.noreply.github.com>
2026-04-20 08:38:46 +0530

    feat: Upgraded network steerer to extreme-performance XDP silicon drop

--- nyx_net.bpf.c
+++ nyx_net.bpf.c
@@ -4,9 +4,8 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/in.h>
-#include <linux/pkt_cls.h>
 
-// Shared Memory Map: The bridge between eBPF and the Wasm Engine
+// Shared Memory Map: The bridge between XDP and the Wasm Engine
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __uint(max_entries, 1);
@@ -14,36 +13,39 @@ struct {
     __type(value, __u32);
 } nyx_packet_map SEC(".maps");
 
-SEC("tc")
-int nyx_tc_ingress(struct __sk_buff *skb) {
-    void *data_end = (void *)(long)skb->data_end;
-    void *data = (void *)(long)skb->data;
+// NEW: Dropping down to XDP SEC
+SEC("xdp")
+int nyx_xdp_ingress(struct xdp_md *ctx) {
+    // We are now dealing with raw silicon memory buffers
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
 
     struct ethhdr *eth = data;
-    if (data + sizeof(*eth) > data_end) return TC_ACT_OK;
+    if (data + sizeof(*eth) > data_end) return XDP_PASS;
 
-    if (eth->h_proto != __constant_htons(ETH_P_IP)) return TC_ACT_OK;
+    if (eth->h_proto != __constant_htons(ETH_P_IP)) return XDP_PASS;
 
     struct iphdr *ip = data + sizeof(*eth);
-    if ((void *)(ip + 1) > data_end) return TC_ACT_OK;
+    if ((void *)(ip + 1) > data_end) return XDP_PASS;
 
     if (ip->protocol == IPPROTO_TCP) {
         struct tcphdr *tcp = (void *)ip + (ip->ihl * 4);
-        if ((void *)(tcp + 1) > data_end) return TC_ACT_OK;
+        if ((void *)(tcp + 1) > data_end) return XDP_PASS;
 
         // ONLY intercept HTTP traffic (Port 80)
         if (tcp->dest == __constant_htons(80) || tcp->source == __constant_htons(80)) {
             __u32 key = 0;
-            // The value we pass is the total length of the packet
-            __u32 val = skb->len; 
+            
+            // NEW: Calculate payload size directly from physical memory bounds
+            __u32 val = (__u32)(data_end - data); 
             
             // Write the packet size into the shared memory bridge
             bpf_map_update_elem(&nyx_packet_map, &key, &val, BPF_ANY);
-            bpf_printk("[NYX-eBPF] Captured HTTP Packet! Size: %d bytes\n", val);
+            bpf_printk("[NYX-XDP] Silicon Drop! HTTP Packet Size: %d bytes\n", val);
         }
     }
 
-    return TC_ACT_OK;
+    return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
